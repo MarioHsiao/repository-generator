4380f599dabdfd81e8ed5d89e052c6034fe97649
Brad Marshall <bmarshall@sped.mobi>
2019-06-06 15:41:34 -0500

    updates

--- RepositoryGenerator.Abstractions/Abstractions/IRepositoryGeneratorServices.cs
+++ RepositoryGenerator.Abstractions/Abstractions/IRepositoryGeneratorServices.cs
@@ -1,26 +1,10 @@
-﻿// // -----------------------------------------------------------------------
-// // <copyright file="IRepositoryGeneratorServices.cs" company="sped.mobi">
-// //     Copyright © 2019 Sped Mobi. All rights reserved.
-// // </copyright>
-// // -----------------------------------------------------------------------
-
-namespace Microsoft.VisualStudio.RepositoryGenerator.Abstractions
+﻿namespace Microsoft.VisualStudio.RepositoryGenerator.Abstractions
 {
     public interface IRepositoryGeneratorServices
     {
-        //IGitHubBlobService Blob { get; }
-
-        //IGitHubCommitService Commit { get; }
-
-        //IGitHubRepositoryTreeService RepositoryTree { get; }
-
-        //IBlobDecoder Decoder { get; }
-
         IReplacementsService Replace { get; }
 
         IRepositoryOpener Opener { get; }
-
-        //IProgressDialogFactory Dialog { get; }
 
         IRepositoryDownloader Downloader { get; }
 
--- RepositoryGenerator.Implementation/Abstractions/Implementation/RepositoryGeneratorServices.cs
+++ RepositoryGenerator.Implementation/Abstractions/Implementation/RepositoryGeneratorServices.cs
@@ -1,10 +1,4 @@
-﻿// // -----------------------------------------------------------------------
-// // <copyright file="RepositoryGeneratorService.cs" company="sped.mobi">
-// //     Copyright © 2019 Sped Mobi. All rights reserved.
-// // </copyright>
-// // -----------------------------------------------------------------------
-
-using System.ComponentModel.Composition;
+﻿using System.ComponentModel.Composition;
 
 namespace Microsoft.VisualStudio.RepositoryGenerator.Abstractions.Implementation
 {
@@ -13,37 +7,20 @@ namespace Microsoft.VisualStudio.RepositoryGenerator.Abstractions.Implementation
     {
         [ImportingConstructor]
         public RepositoryGeneratorServices(
-            //[Import] IGitHubBlobService blob,
-            //[Import] IGitHubCommitService commit,
-            //[Import] IGitHubRepositoryTreeService repositoryTree,
-            //[Import] IBlobDecoder decoder,
             [Import] IReplacementsService replace,
             [Import] IRepositoryOpener opener,
-            //[Import] IProgressDialogFactory dialog,
             [Import] IRepositoryDownloader downloader,
             [Import] IRepositoryExpander expander)
         {
-            //Blob = blob;
-            //Commit = commit;
-            //RepositoryTree = repositoryTree;
-            //Decoder = decoder;
             Replace = replace;
             Opener = opener;
-            //Dialog = dialog;
             Downloader = downloader;
             Expander = expander;
         }
 
-        //public IGitHubBlobService Blob { get; }
-        //public IGitHubCommitService Commit { get; }
-        //public IGitHubRepositoryTreeService RepositoryTree { get; }
-        //public IBlobDecoder Decoder { get; }
         public IReplacementsService Replace { get; }
         public IRepositoryOpener Opener { get; }
-        //public IProgressDialogFactory Dialog { get; }
-
         public IRepositoryDownloader Downloader { get; }
-
         public IRepositoryExpander Expander { get; }
     }
 }
